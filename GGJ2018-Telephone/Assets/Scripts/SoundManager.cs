be2af0674e3cf7c2ae8c83d836d65dc3d6a0d6ea
bahamute <elore@me.com>
2018-01-28 03:52:20 +0900

    sound update
    book, speech sound

--- GGJ2018-Telephone/Assets/Scripts/SoundManager.cs
+++ GGJ2018-Telephone/Assets/Scripts/SoundManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
@@ -8,7 +9,12 @@ public class SoundManager : MonoBehaviour
 
     public AudioSource switchOn, switchOff;
     public AudioSource connectorOn, connectorOff;
-    public AudioSource page;
+    public AudioSource page, bookOpen, bookClose;
+
+    public AudioSource speech;
+    public AudioClip[] speechClips;
+
+    public AudioMixerSnapshot normalShot, machineShot;
 
     void Awake()
     {
@@ -43,4 +49,26 @@ public class SoundManager : MonoBehaviour
     {
         page.Play();
     }
+
+    public void bookSound(bool onoff)
+    {
+        if (onoff)
+        {
+            bookOpen.Play();
+            machineShot.TransitionTo(1f);
+        }
+        else
+        {
+            bookClose.Play();
+            normalShot.TransitionTo(1f);
+        }
+    }
+
+    public void speechSound(int index)
+    {
+        speech.clip = speechClips[index];
+        speech.pitch = Random.Range(0.95f, 1.05f);
+        speech.Play();
+    }
+
 }
