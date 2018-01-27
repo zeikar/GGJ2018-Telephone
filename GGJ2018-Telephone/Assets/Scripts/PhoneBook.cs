72544e5822fffeb0d99f6ee9948a936f82b013c4
Zeikar <antstp@naver.com>
2018-01-27 13:03:50 +0900

    add phonebook button

---
+++ GGJ2018-Telephone/Assets/Scripts/PhoneBook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneBook : MonoBehaviour
+{
+    bool isActive;
+
+    // Use this for initialization
+    void Start()
+    {
+        isActive = false;
+        this.gameObject.SetActive(isActive);
+    }
+    
+    public void PhoneBookToggle()
+    {
+        isActive = !isActive;
+
+        this.gameObject.SetActive(isActive);
+    }
+}
